168a4971afc55bacd6b81dec15f8561c12e31adc
Anthony Bernardo Kamers <anthony.kamers@ufsc.br>
2022-03-11 21:48:14 -0300

    first

---
+++ app.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var path = require('path');
+var express = require('express');
+var app = express();
+
+// app.get('/', express.static(path.join(__dirname, "public")));
+app.get('/', (req, res) => {
+    res.send(`<h1>Hello, it's encrypted</h1>`);
+})
+
+module.exports = app;
+
+// for development and debugging
+if (require.main === module) {
+    require('http').createServer(app).listen(3000, function () {
+        console.info("Listening for HTTP on", this.address());
+    });
+}
